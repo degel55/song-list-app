f0b26d1feff9f9b7fc05001f73b5134324ab9156
degel55 <chicubfan@gmail.com>
2016-08-19 23:56:17 -0500

    update song file

---
+++ song.rb
@@ -0,0 +1,24 @@
+class Song
+
+  def initialize(title, artist, duration, lyrics)
+    @title = title
+    @artist = artist
+    @duration = duration
+    @lyrics = lyrics
+  end
+  
+  def title
+    return @title
+  end   
+
+  def artist
+    return @artist
+  end 
+
+  def duration
+    return @duration
+  end 
+
+  def lyrics
+    return @lyrics
+  end  
